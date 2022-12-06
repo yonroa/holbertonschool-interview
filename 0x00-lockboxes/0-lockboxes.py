6f17e53d453410d9109298aa55c3559b033fbd55
yonroa <yonanderson11@gmail.com>
2022-12-06 12:42:11 -0500

    Add unlock_box

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Contanins the 'canUnlockAll' function"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    key_box = [0]
+    attemp = 0
+    while attemp != len(boxes):
+        for keys in key_box:
+            for key in boxes[keys]:
+                if key not in key_box:
+                    key_box.append(key)
+        attemp += 1
+    if len(key_box) == len(boxes):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
