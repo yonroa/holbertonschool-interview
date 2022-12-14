af3d9fbee533de127df8514f0fddb67ef1c3f1d8
yonroa <yonanderson11@gmail.com>
2022-12-14 14:30:51 -0500

    Update task

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -22,7 +22,16 @@ listint_t *insert_node(listint_t **head, int number)
     new->next = NULL;
 
     if (*head == NULL)
+    {
         *head = new;
+        return (new);
+    }
+    if ((*head)->n > number)
+    {
+        new->next = *head;
+        *head = new;
+        return (new);
+    }
     else
     {
         while (current->next != NULL && current->next->n < number)
