bc8811e899a8955c099c4bcadb6641f888911afa
yonroa <yonanderson11@gmail.com>
2023-01-26 11:47:03 -0500

    add sorted to keys

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,41 +3,41 @@
 
 import sys
 
-status_code = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-counter = 1
-size = 0
-
-
-def parse_line(line):
-    """Read and grab data"""
-    try:
-        parse = line.split()
-        status = parse[-2]
-        if status in status_code.keys():
-            status_code[status] += 1
-        return int(parse[-1])
-    except Exception:
-        return 0
-
-
-def print_stats():
-    """Print the stats"""
-    print("File size: {}".format(size))
-    for key in status_code.keys():
-        if status_code[key]:
-            print("{} {}".format(key, status_code[key]))
-
 
 if __name__ == "__main__":
+    status_code = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+    counter = 1
+    size = 0
+
+
+    def parse_line(line):
+        """Read and grab data"""
+        try:
+            parse = line.split()
+            status = parse[-2]
+            if status in sorted(status_code.keys()):
+                status_code[status] += 1
+            return int(parse[-1])
+        except Exception:
+            return 0
+
+
+    def print_stats():
+        """Print the stats"""
+        print("File size: {}".format(size))
+        for key in status_code.keys():
+            if status_code[key]:
+                print("{} {}".format(key, status_code[key]))
+
     try:
         for line in sys.stdin:
             size += parse_line(line)
