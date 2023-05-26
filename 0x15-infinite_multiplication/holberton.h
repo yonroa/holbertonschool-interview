6f6684a7d9e18346eca88c6c3091d0e6418420bb
yonroa <yonanderson11@gmail.com>
2023-05-25 19:44:28 -0500

    Upload task

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <ctype.h>
+
+int _isnumber(char *s);
+char *_callocX(unsigned int nmemb);
+int _putchar(char c);
+
+#endif
